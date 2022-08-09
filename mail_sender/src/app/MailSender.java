9697138bdcd6add49d44214cb9009e0cc0553944
kycasdzxc <105793579+kycasdzxc@users.noreply.github.com>
2022-08-09 23:03:00 +0900

    이메일 보내기

---
+++ mail_sender/src/app/MailSender.java
@@ -0,0 +1,44 @@
+package app;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class MailSender {
+	public static void main(String[] args) {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+		
+		Session session = Session.getInstance(props, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("yermi0001@gmail.com", "ntkkbyswowynihgg");
+			}
+		});
+		
+		String receiver = "kycasdzxc@naver.com";
+		String title = "테스트 메일입니다.";
+		String content = "<h2 style='color:blue'>안녕하세요</h2>";
+		Message message = new MimeMessage(session);
+		try {
+			message.setFrom(new InternetAddress("kycasdzxc@gmail.com", "관리자", "utf-8"));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
+			message.setSubject(title);
+			message.setContent(content, "text/html; charset=utf-8");
+
+			Transport.send(message);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
